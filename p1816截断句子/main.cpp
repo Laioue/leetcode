1ce448ed1276d2786203fad90e19df084efb583a
Laioue <liukai.ai@outlook.com>
2021-12-06 21:16:16 +0800

    截断句子

--- p1816截断句子/main.cpp
+++ p1816截断句子/main.cpp
@@ -1,13 +1,30 @@
 //
 // Created by liukai on 2021/12/6.
 //
-
 #include "iostream"
-
+#include "string"
 using namespace std;
+
+
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+
+        for (int i=0;i<s.length();i++) {
+            if (s[i] == ' ')
+                k--;
+            if (k == 0)
+                return s.substr(0, i);
+        }
+        return s;
+    }
+};
 
 int main()
 {
-    cout << "test";
+    string s = "Hello how are you Contestant";
+    int k = 4;
+    Solution solution;
+    cout << solution.truncateSentence(s, k);
     return 0;
 }
